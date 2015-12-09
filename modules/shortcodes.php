4d5c38eb8a7578ea36844c67f46c0fd7ff40247d
Mike Selander <mikeselander@gmail.com>
2015-12-08 19:24:25 -0700

    Added base shortcode file - default includes button shortcode

---
+++ modules/shortcodes.php
@@ -0,0 +1,34 @@
+<?php
+namespace evans;
+
+/*
+ * Create a button for the editor.
+ *
+ * @param array $atts Attributes
+ * @return string HTML output
+ */
+add_shortcode( 'button', __NAMESPACE__ . '\button_shortcode' );
+function button_shortcode( $atts ){
+
+	$atts = shortcode_atts(
+		array(
+			'link'		=> '',
+			'text'		=> 'Read More',
+			'target'	=> '',
+		),
+		$atts
+	);
+
+	// Make sure we're actually linking somewhere
+	if ( empty( $atts['link'] ) ){
+		return;
+	}
+
+	$link = "<a href='".esc_url( $atts['link'] )."'";
+		if ( !empty( $atts['target'] ) ){ $link .= " target='".esc_attr(  $atts['target'] )."'"; }
+		$link .= esc_attr( $atts['text'] );
+	$link .="</a>";
+
+	return $link;
+
+}
