9dd75064d7215f82f1dcb286c39cb0fc8980c06c
Mike Selander <mikeselander@gmail.com>
2016-01-02 10:14:14 -0700

    Broke up the create test content into multiple AJAX calls instead of the single & improved error reporting

--- site-setup/class-test-data-build.php
+++ site-setup/class-test-data-build.php
@@ -64,7 +64,7 @@ class BuildTestData{
 
 		} elseif ( $action == 'create' ){
 
-			$this->create_post_type_content( $cptslug, true );
+			$this->create_post_type_content( $cptslug, true, 1 );
 
 		}
 
@@ -118,7 +118,7 @@ class BuildTestData{
 
 					jQuery( '.handle-test-data' ).on( 'click', function(){
 
-						$( this ).after( '<img src="<?php echo plugins_url( '../assets/images/loading.gif', __FILE__ ); ?>" class="loading-icon" style="height: 20px; margin-bottom: -4px; margin-left: 4px;">' );
+						jQuery( this ).after( '<img src="<?php echo plugins_url( '../assets/images/loading.gif', __FILE__ ); ?>" class="loading-icon" style="height: 20px; margin-bottom: -4px; margin-left: 4px;">' );
 
 						var data = {
 							'action' : 'handle_test_data',
@@ -127,10 +127,22 @@ class BuildTestData{
 							'nonce' : '<?php echo wp_create_nonce( 'handle-test-data' ); ?>'
 						};
 
-						jQuery.post( ajaxurl, data, function(response) {
-							console.log( '<?php __( 'Got this from the server: ', 'evans-mu' ); ?>' + response );
-							$( '.loading-icon' ).remove();
-						});
+						if ( jQuery( this ).data( 'todo' ) == 'create' ){
+							var count = Math.floor( ( Math.random() * 30 ) + 1 );
+						} else {
+							var count = 1;
+						}
+
+						for( var i=1; i<=count; i++ ){
+
+							jQuery.post( ajaxurl, data, function(response) {
+								console.log( '<?php __( 'Got this from the server: ', 'evans-mu' ); ?>' + response );
+							});
+
+						}
+
+						jQuery( '.loading-icon' ).remove();
+						console.log( 'Creating ' + count + ' objects' );
 
 					});
 
@@ -177,7 +189,7 @@ class BuildTestData{
 		}
 
 		// Create test posts
-		for( $i = 1; $i <= $num; $i++ ){
+		for( $i = 0; $i < $num; $i++ ){
 
 			$return = $this->create_test_object( $cptslug, $supports, $metaboxes );
 
@@ -186,8 +198,6 @@ class BuildTestData{
 			}
 
 		}
-
-		echo "Created $i objects";
 
 	}
 
@@ -302,6 +312,7 @@ class BuildTestData{
 	 * @param array $supports All CMB2 metaboxes attached to the post type.
 	 */
 	private function create_test_object( $cptslug, $supports, $metaboxes ){
+		$return = '';
 
 		// Get a random title
 		$title = TestContent::title();
@@ -336,17 +347,25 @@ class BuildTestData{
 			 update_post_meta( $post_id, '_thumbnail_id', TestContent::image( $post_id ) );
 		}
 
+		$taxonomies = get_object_taxonomies( $cptslug );
+
+		// Assign the post to terms
+		if ( !empty( $taxonomies ) ){
+			$return .= $this->assign_terms( $post_id, $taxonomies );
+		}
+
 		// Spin up metaboxes
 		if ( !empty( $metaboxes ) ){
 			foreach ( $metaboxes as $cmb ) :
-				$return = $this->random_metabox_content( $post_id, $cmb );
+				$return .= $this->random_metabox_content( $post_id, $cmb );
 			endforeach;
 		}
 
+		// Check if we have errors and return them or created message
 		if ( is_wp_error( $return ) ){
 			return $return;
 		} else {
-			return "Created ".get_post_type( $post_id )." $post_id: ".admin_url( '/post.php?post='.$post_id.'&action=edit' )."
+			return "Created " . get_post_type( $post_id ) . " $post_id: " . admin_url( '/post.php?post='.$post_id.'&action=edit' ) . "
 ";
 		}
 
@@ -372,6 +391,61 @@ class BuildTestData{
 		);
 
 		return $supports;
+
+	}
+
+
+	/**
+	 * Assigns taxonomies to the new post.
+	 *
+	 * Loop through every taxonomy type associated with a custom post type &
+	 * assign the post to a random item out of each taxonomy. Taxonomies must
+	 * have at least one term in them for this to work.
+	 *
+	 * @access private
+	 *
+	 * @param int $post_id a custom post type ID.
+	 * @param array $taxonomies taxonomies assigned to this cpt.
+	 * @return object WP Error if there is one.
+	 */
+	private function assign_terms( $post_id, $taxonomies ){
+
+		// Make sure it's an array & has items
+		if ( empty( $taxonomies ) || !is_array( $taxonomies ) ){
+			return;
+		}
+
+		foreach ( $taxonomies as $tax ){
+
+			// Get the individual terms already existing
+			$terms = get_terms( $tax, array( 'hide_empty'	=> false ) );
+			$count = count( $terms ) - 1;
+
+			// If there are no terms, skip to the next taxonomy
+			if ( empty( $terms ) ){
+				continue;
+			}
+
+			// Get a random index to use
+			$index = rand( 0, $count );
+
+			// Initialize our array
+			$post_data = array(
+				'ID'	=> $post_id
+			);
+
+			// Set the term data to update
+			$post_data['tax_input'][ $tax ] = array( $terms[$index]->term_id );
+
+			// Update the post with the taxonomy info
+			$return = wp_update_post( $post_data );
+
+			// Return the error if it exists
+			if ( is_wp_error( $return ) ){
+				return $pid->get_error_messages();
+			}
+
+		}
 
 	}
 
